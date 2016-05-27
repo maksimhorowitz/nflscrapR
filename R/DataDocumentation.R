fb8d734bd556d1546136434cb5aab958df104fb1
maksimhorowitz <mahorowi@andrew.cmu.edu>
2016-05-27 16:10:27 -0400

    Fixing bugs

---
+++ R/DataDocumentation.R
@@ -0,0 +1,11 @@
+################################################################## 
+### Documenting the data files                                 ###
+# Author: Maksim Horowitz                                        #
+# Code Style Guide: Google R Format                              #
+################################################################## 
+
+
+#' NFL Team Names and Abbreviations
+#' @format This dataset contains the full team names and abbreviations
+#' of each of the 32 teams in the NFL.  The data set has 32 rows and 2 columns
+"nflteams"
