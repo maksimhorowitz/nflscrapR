f7d3c235644f0cad7509379bd7e9d04301cd75a9
maksimhorowitz <mahorowi@andrew.cmu.edu>
2018-01-27 20:12:22 -0500

    Updated description file.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,2 +1,31 @@
 library(testthat)
 library(nflscrapR)
+
+# Testing why OT is not being scraped
+
+OTgame <- game_play_by_play(2017092403)
+
+OTgame09 <- game_play_by_play(2009100401)
+
+games2017 <- season_games(2017)
+
+bal_rost_17 <- season_rosters(2017, "BAL", "RUNNING_BACK")
+
+data.frame(colnames(OTgame09),
+           sapply(OTgame09, class)) %>%
+  write.csv(file = "/Users/maksimhorowitz/Documents/PBP_Columns.csv")
+
+urlstring <- proper_jsonurl_formatting(GameID)
+
+nfl_api_raw <- RJSONIO::fromJSON(RCurl::getURL(urlstring))
+
+names(nfl_api_raw[[1]]$home$stats)
+
+
+data.frame(do.call(rbind,nfl_api_raw[[1]]$drives[[1]]$plays)) %>% View
+
+
+data.frame(do.call(rbind,nfl_api_raw[[1]][[3]][[1]][["plays"]][[3]][["players"]][[2]])) %>% View
+
+nfl_api_raw[[1]][[3]][[1]][["plays"]][[4]][["players"]] %>% names
+
